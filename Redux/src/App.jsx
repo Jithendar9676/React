0b49c249659c868a7b34b0544e318393e05a9e1e
jithendar9676 <jithendarreddy9676@gmail.com>
2024-08-23 01:15:57 +0530

    Redux

---
+++ Redux/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import TodoForm from '../../TodoContextLocalStorage/src/Component/TodoForm'
+import Todos from './Component/Todos'
+
+function App() {
+
+
+  return (
+    <>
+      <TodoForm/>
+      <Todos/>
+    </>
+  )
+}
+
+export default App
