a507d4c8efceec89eddb0bcfbb847c3169cc4c32
jithendar9676 <jithendarreddy9676@gmail.com>
2024-08-27 22:13:10 +0530

    api

--- API_Handle/src/App.jsx
+++ API_Handle/src/App.jsx
@@ -1,10 +1,32 @@
-
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 import './App.css'
-import customQuery from './customQuery'
+
 
 
 function App() {
- const [data,error,loading] =customQuery('https://dummyjson.com/products')
+  const [data, setData] = useState([])
+  const [error, setError] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const [search, setSearch] = useState()
+  useEffect(() => {
+
+    (async () => {
+      try {
+        setLoading(true)
+        setError(false)
+        const res = await axios.get('https://dummyjson.com/products')
+        const datas = res.data?.products
+        setData(datas)
+        setLoading(false)
+      } catch (error) {
+        setError(true)
+        setLoading(false)
+      }
+    })()
+
+
+  }, [])
 
   // if(loading){
   //   return(
@@ -17,22 +39,82 @@ function App() {
   //     <div>Something went wrong</div>
   //   )
   // }
-  console.log("product",data)
+  console.log("product", data)
+  let count = 0
+  const betterFunction = () => {
+    console.log("fecting --", ++count)
+  }
+  const DeBounes = (fn, dely) => {
+    let timer = 0
+    return function () {
+      let context = this
+      let args = arguments
+      clearTimeout(timer)
+      timer = setTimeout(() => {
+        fn.apply(context, args)
+      }, dely);
+    }
+  }
+  const getData = DeBounes(betterFunction, 300)
+
+  const handleclick = async () => {
+    try {
+      console.log("clicked.......")
+      const res = await axios.get("https://dummyjson.com/prod")
+      console.log(res.data)
+
+    } catch (error) {
+
+    }
+
+  }
+
+  // function trotle(fn,dely){
+  //   let shouldCall=true
+  //   return function(...args){
+  //     if(!shouldCall)
+  //       return;
+  //      fn(...args)
+  //      shouldCall=false
+  //      setTimeout(()=>{
+  //        shouldCall=true
+  //      },dely)
+  //   }
+  // }
+
+  function trotle(fn, dely) {
+    let lastCall = 0
+    return function (...args) {
+      const now = new Date().getTime()
+      if ((now - lastCall) < dely) {
+        return;
+      }
+      lastCall = now
+      fn(...args)
+    }
+  }
+
+
 
   return (
     <>
-    
-     <div>Api Handle</div>
-     {loading && <div>loading!!!</div>}
-    {error && <div>some went wrong!!!</div>}
-     <div>Total Products : {data.length}</div>
-    {
-      data.map((e)=>(
-           
-        <h1 key={e.id}>{e.title}</h1>
-      ))
-    }
-     
+      <div>
+        <input type="text" onKeyUp={getData} />
+        <button onClick={trotle(handleclick, 1000)}
+
+        >Click</button>
+      </div>
+      <div>Api Handle</div>
+      {loading && <div>loading!!!</div>}
+      {error && <div>some went wrong!!!</div>}
+      <div>Total Products : {data.length}</div>
+      {
+        data.map((e) => (
+
+          <h1 key={e.id}>{e.title}</h1>
+        ))
+      }
+
     </>
   )
 }
